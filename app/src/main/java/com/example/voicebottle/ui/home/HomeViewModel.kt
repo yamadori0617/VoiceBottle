2e534734f6c68d81f993a556825c35c19fa19d83
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-04-08 18:05:20 +0900

    first commit

---
+++ app/src/main/java/com/example/voicebottle/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.voicebottle.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragments"
+    }
+    val text: LiveData<String> = _text
+}
