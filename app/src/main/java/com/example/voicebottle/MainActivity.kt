a71d9cf7b1e7c145b5d6047dada94bfaf5f5e554
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-04-14 20:05:25 +0900

    update record

--- app/src/main/java/com/example/voicebottle/MainActivity.kt
+++ app/src/main/java/com/example/voicebottle/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.voicebottle
 
 import android.Manifest
-import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.os.Environment
+import android.os.Environment.DIRECTORY_MUSIC
+import android.os.Environment.DIRECTORY_PICTURES
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.emrekose.recordbutton.RecordButton
 import com.example.voicebottle.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.io.File
+import java.nio.file.Files
 
 
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+private const val AUDIO_RECORD_DIR = "/AudioRecord"
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
@@ -24,11 +29,12 @@ class MainActivity : AppCompatActivity() {
     private var permissionToRecordAccepted = false
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
+    private lateinit var player: MediaPlayer
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
@@ -52,11 +58,33 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_record, R.id.navigation_reply))
+        val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                        R.id.navigation_home, R.id.navigation_record, R.id.navigation_reply
+                )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        player = MediaPlayer.create(this, R.raw.sound_waves)
+        player.isLooping = true
+        player.setVolume(0.22F, 0.22F)
+
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+
+        val audioRecordDir = File(filesDir, "/AudioRecord")
+        if(!audioRecordDir.exists()) {
+            audioRecordDir.mkdirs()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        player.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player.pause()
     }
 }
