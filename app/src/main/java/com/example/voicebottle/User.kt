6d4a5fc51ac4e69b95f262c44b157a10201937ac
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-04-30 00:20:39 +0900

    update User.kt

--- app/src/main/java/com/example/voicebottle/User.kt
+++ app/src/main/java/com/example/voicebottle/User.kt
@@ -1,4 +1,12 @@
 package com.example.voicebottle
 
-class User {
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class User : RealmObject() {
+    @PrimaryKey
+    var user_id: String = ""
+    var password: String = ""
+    var name: String = ""
+    var api_token: String = ""
 }
