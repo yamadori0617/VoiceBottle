2a4f59b0f90bdf7144ff4ea6b53890e538d2fbef
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-12 18:07:38 +0900

    update ProfileFragment.kt

--- app/src/main/java/com/example/voicebottle/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/voicebottle/ui/profile/ProfileFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.voicebottle.User
 import com.example.voicebottle.databinding.FragmentProfileBinding
+import io.realm.Realm
+import io.realm.kotlin.where
 
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        realm = Realm.getDefaultInstance()
     }
 
     override fun onCreateView(
@@ -23,6 +27,7 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.myNameText.text = realm.where<User>().findFirst()?.user_name
         return binding.root
     }
 
@@ -33,5 +38,6 @@ class ProfileFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
+        realm.close()
     }
 }
