6dc02090d80dabbe76f99f86b29d62c80619d01f
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-20 19:13:06 +0900

    update ApiService.kt

--- app/src/main/java/com/example/voicebottle/ApiService.kt
+++ app/src/main/java/com/example/voicebottle/ApiService.kt
@@ -31,14 +31,10 @@ class RestApiService {
                 object : Callback<RegUser> {
                     override fun onFailure(call: Call<RegUser>, t: Throwable) {
                         onResult(null)
-                        println("onFailure")
-                        println(t.message)
                     }
                     override fun onResponse( call: Call<RegUser>, response: Response<RegUser>) {
                         val addedUser = response.body()
                         onResult(addedUser)
-                        println("onResponse")
-                        println(addedUser)
                     }
                 }
         )
@@ -70,8 +66,6 @@ class RestApiService {
                 override fun onResponse(call: Call<GetMessage>, response: Response<GetMessage>) {
                     val gotMessage = response.body()
                     onResult(gotMessage)
-                    println("onResponse")
-                    println(gotMessage)
                 }
             }
         )
