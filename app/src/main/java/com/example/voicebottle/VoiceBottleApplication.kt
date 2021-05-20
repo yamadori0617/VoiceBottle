1cf875b28e5a7fe52e833e21bc77b1fc56c68ff8
yamadori0617 <51155766+yamadori0617@users.noreply.github.com>
2021-05-20 19:47:40 +0900

    Update VoiceBottleApplication.kt

--- app/src/main/java/com/example/voicebottle/VoiceBottleApplication.kt
+++ app/src/main/java/com/example/voicebottle/VoiceBottleApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
+
 class VoiceBottleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
